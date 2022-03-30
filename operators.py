b6f5033f59ded114285245eb19f27a60a90c363d
Mohammed AboHashem <36199398+abohashem95@users.noreply.github.com>
2022-03-30 06:11:59 +0200

    Add files via upload
    This file contain python comments that explain  python operators

---
+++ operators.py
@@ -0,0 +1,63 @@
+'''
+There are 3 types of operators 
+
+1- Arithmatic operators
+2- comparision operators
+3- Assignment operators 
+
+
+-----------------------------------
+
+1- Arithmatic operators 
+
+(+) Addition        # 5+6	sol = 11
+(-) subtraction     # 4-2	sol = 2
+(*) mutiplication	# 3*4 	sol = 12
+(/) devision 		# 8/4 	sol = 2
+
+(%) module: devide left hand operator by right hand and return remainder  # 9%4 sol = 1
+(**) exponent		# 2**3  sol = 8
+(//) floor devision: devision with removing decimal point   			  # 7//2 sol = 3.0
+
+----------------------------------
+
+2- comparison operators
+
+## assume a = 10 ,  b = 20 ## 
+
+(==) equal						# (a == b) is not true
+(!=) not equal					# (a != b) is true
+(<>) not equal 					#(a <> b) is true  note: similar to (!=)
+(>)	 greater than				# (a > b) is not true
+(<)	 less than					# (a < b) is true
+(>=) greater than or equal		# (a >= b) is not true
+(<=) less than or equal			# (a <= b) is true
+
+----------------------------------
+
+3- assignment operators
+
+
+(=)   		# c = a + b
+(+=)		# c += a  ---> c = c + a
+(-=)		# c -= a  ---> c = c - a
+(*=)		# c *= a  ---> c = c * a
+(/=)		# c /= a  ---> c = c / a
+(%=)		# c %= a  ---> c = c % a
+(**=)		# c **= a  ---> c = c ** a
+(//=)		# c //= a  ---> c = c // a
+
+
+--------------------------------------
+
+
+## operator precenddenc ##
+
+
+1- ()
+2- **
+3- */
+4- +-
+
+
+'''
